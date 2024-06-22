03f7dbeb9101e336eff61bab15322dc2de247913
chagchagchag <chagchagchag.dev@gmail.com>
2024-06-22 10:05:03 +0900

    first status

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Docs Spring Batch</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/docs-spring-batch',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/docs-spring-batch',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
